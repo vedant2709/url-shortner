dd6b8c2c100f06d9b3e23f7a38566da9b12a0761
Vedant Chaudhary <vedantchaudhary2002@gmail.com>
2024-07-31 18:59:22 +0530

    working on home view

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
+const path=require('path')
 const urlRoute=require("./routes/url")
+const staticRoute=require("./routes/staticRouter")
 const PORT=8000;
 const {connectToMongoDB}=require("./connect")
 const URL =require("./models/url")
@@ -8,9 +10,13 @@ const URL =require("./models/url")
 connectToMongoDB("mongodb://127.0.0.1:27017/url-shortner")
 .then(()=>console.log("MonogoDB connected"))
 
+app.set('view engine','ejs')
+app.set("views",path.resolve("./views"))
+
 app.use(express.json())
 
 app.use("/url",urlRoute)
+app.use("/",staticRoute)
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
